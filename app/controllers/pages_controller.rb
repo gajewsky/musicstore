40ab76cd9a002dea41cdb01dca10085acf8b1793
Marcin Gajewski <marcin.gajewski@polcode.net>
2015-03-17 12:37:07 +0100

    add dashboard action to pages controller\

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: [:dashboard]
   def home
     redirect_to albums_path if current_user
+  end
+
+  def dashboard
+    @albums = current_user.albums
   end
 end
