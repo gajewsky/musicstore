b79762d406e629fc07b3fc6f13bd6560dfb78099
Marcin Gajewski <marcin.gajewski@polcode.net>
2015-03-17 11:04:27 +0100

    add user album association

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,4 +1,5 @@
 class AlbumsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
   before_action :set_album, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -10,43 +11,26 @@ class AlbumsController < ApplicationController
 
   def new
     @album = Album.new
+    respond_with(@album)  
   end
 
   def edit
   end
 
   def create
-    @album = Album.new(album_params)
-
-    respond_to do |format|
-      if @album.save
-        format.html { redirect_to @album, notice: 'album was successfully created.' }
-        format.json { render :show, status: :created, location: @album }
-      else
-        format.html { render :new }
-        format.json { render json: @album.errors, status: :unprocessable_entity }
-      end
-    end
+    @album = current_user.albums.new(album_params)
+    @album.save
+    respond_with(@album)   
   end
 
   def update
-    respond_to do |format|
-      if @album.update(album_params)
-        format.html { redirect_to @album, notice: 'album was successfully updated.' }
-        format.json { render :show, status: :ok, location: @album }
-      else
-        format.html { render :edit }
-        format.json { render json: @album.errors, status: :unprocessable_entity }
-      end
-    end
+    @album.update(album_params)
+    respond_with(@album)    
   end
 
   def destroy
     @album.destroy
-    respond_to do |format|
-      format.html { redirect_to albums_url, notice: 'album was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    respond_with(@album)  
   end
 
   private
